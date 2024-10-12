9d5fbadbfacb58fdc5f41cb568501bdd81294d46
Ritikesh <ritikeshyadav2020@gmail.com>
2024-10-12 13:58:04 +0530

    completed miscellaneous module

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import courseRoutes from './routes/course.routes.js';
 import cookieParser from "cookie-parser";
 import paymentRoutes from './routes/payment.routes.js';
+import miscellaneousRoutes from './routes/miscellaneous.routes.js';
 
 
 const app=express();
@@ -34,6 +35,7 @@ app.get('/ping',(req,res)=>{
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/courses', courseRoutes);
 app.use('/api/v1/payments',paymentRoutes);
+app.use('/api/v1',miscellaneousRoutes);
 
 app.all("*",(req,res)=>{
 	res.status(404).send("OOOPS!! Page not found ");
