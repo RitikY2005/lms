ea516acf8316c2d65fbf735304ab8645be1fd828
ritik2005 <131939663+RitikY2005@users.noreply.github.com>
2024-10-14 22:28:45 +0530

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 
-import {FaFacebook,FaInstagram,FaLinkedIn,FaTwitter} from 'react-icons/fa';
+import {FaFacebook,FaInstagram,FaLinkedin,FaTwitter} from 'react-icons/fa';
 
 
 function Footer(){
@@ -7,18 +7,18 @@ function Footer(){
 	const currentYear= date.getFullYear();
 
   return (
-  	<footer className="width-full flex flex-col sm:flex-row justify-between align-center h-[10vh] relative left-0 bottom-0 py-4 px-2 bg-gray-500">
-       <section clasName="">
+  	<footer className="width-full flex flex-col sm:flex-row justify-between items-center h-[8vh] relative left-0 bottom-0 px-12 bg-gray-700 ">
+       <section className="flex justify-center align-center">
          <p className="text-white text-lg ">
-         	{currentYear} &copy; all rights reserved .
+         	Learning Pvt.ltd {currentYear} &copy; all rights reserved .
          </p>
        </section>
 
        <section className="flex justify-center align-center space-x-4">
-       	  <FaFacebook/>
-       	  <FaInstagram/>
-       	  <FaLinkedIn/>
-       	  <FaTwitter/>
+       	  <FaFacebook className='cursor-pointer text-white hover:scale-110 transition-all duration-300 ease-in-out ' size={"22px"}/>
+       	  <FaInstagram className='cursor-pointer text-white hover:scale-110 transition-all duration-300 ease-in-out ' size={"22px"}/>
+       	  <FaLinkedin className='cursor-pointer text-white hover:scale-110 transition-all duration-300 ease-in-out ' size={"22px"}/>
+       	  <FaTwitter className='cursor-pointer text-white hover:scale-110 transition-all duration-300 ease-in-out ' size={"22px"}/>
        </section>
   	</footer>
   	)
